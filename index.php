a46ce446382eab1278d875a834103391f4d90c41
Andrey Koptug <domasedd@yandex.ru>
2017-09-28 10:45:19 +0300

    Realization on NetBeans
    Добавлен тестовый вариант парсера через API vk.

--- index.php
+++ index.php
@@ -1,86 +1,113 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Neo4JKEK</h1>
+
 <?php
-/**
- * To install Neo4j-PHP-Client, we use Composer
- *
- * $ curl -sS https://getcomposer.org/installer | php
- * $ php composer.phar require graphaware/neo4j-php-client
- *
- */
+//запрос по API к vk
+$permissions = [
+  'notify','friends','photos','audio','video','docs','pages','status','wall',
+    'groups','messages','email','stats'
+    
+];
+$request_params = [
+    'client_id' => '6199555',
+    'redirect_url' => 'https://oauth.vk.com/blank.html',
+    'response_type' => 'token',
+    'display' => 'page',
+    'scope' => implode(',', $permissions)
 
-require __DIR__.'/vendor/autoload.php';
+    
+];
+ $url = 'https://oauth.vk.com/authorize?' .  http_build_query($request_params);  
 
-use GraphAware\Neo4j\Client\ClientBuilder;
-
-// change to your hostname, port, username, password
-$neo4j_url = "bolt://neo4j:azizakek@localhost";
-
-// setup connection
-$client = ClientBuilder::create()
-    ->addConnection('default',  $neo4j_url)
-    ->build();
-
-// setup data
-$insert_query = <<<EOQ
-UNWIND {pairs} as pair
-MERGE (p1:Person {name:pair[0]})
-MERGE (p2:Person {name:pair[1]})
-MERGE (p1)-[:KNOWS]-(p2);
-EOQ;
-
-// friend data to insert
-$data = [["Jim","Mike"],["Jim","Billy"],["Anna","Jim"],
-    ["Anna","Mike"],["Sally","Anna"],["Joe","Sally"],
-    ["Joe","Bob"],["Bob","Sally"]];
-
-// insert data
-$client->run($insert_query, ["pairs" => $data]);
+//echo $url;
 
 
-// friend of friend: query
-$foaf_query = <<<EOQ
-MATCH (person:Person)-[:KNOWS]-(friend)-[:KNOWS]-(foaf)
-WHERE person.name = {name}
-  AND NOT (person)-[:KNOWS]-(foaf)
-RETURN foaf.name AS name
-EOQ;
-
-// friend of friend: build and execute query
-$params = ['name' => 'Joe'];
-$result = $client->run($foaf_query, $params);
-
-foreach ($result->records() as $record) {
-    echo $record->get('name') . PHP_EOL;
-}
-
-
-// common friends: query
-$common_friends_query = <<<EOQ
-MATCH (user:Person)-[:KNOWS]-(friend)-[:KNOWS]-(foaf:Person)
-WHERE user.name = {user} AND foaf.name = {foaf}
-RETURN friend.name AS friend
-EOQ;
-
-// common friends: build and execute query
-$params = ['user' => 'Joe', 'foaf' => 'Sally'];
-$result = $client->run($common_friends_query, $params);
-
-foreach ($result->records() as $record) {
-    echo $record->get('friend') . PHP_EOL;
-}
-
-
-// connecting paths: query
-$connecting_paths_query = <<<EOQ
-MATCH path = shortestPath((p1:Person)-[:KNOWS*..6]-(p2:Person))
-WHERE p1.name = {name1} AND p2.name = {name2}
-RETURN [n IN nodes(path) | n.name] as names
-EOQ;
-
-// connecting paths: build and execute query
-$params = ['name1' => 'Joe', 'name2' => 'Billy'];
-$result = $client->run($connecting_paths_query, $params);
-
-foreach ($result->records() as $record) {
-    print_r($record->get('names'));
-}
+//token 62730d1ed3005f721d56bb05bb234b6fc9e45dcd66acb7c45f47a6f0a3396a7e8322ca0b0ffe9f2a9aaf4
+// начало запроса по группам сообщества
+ 
+ 
+ $fields = ['connections', 'site','education','contacts','photo_max','status','city'];
+ $request_par = [
+     'group_id'=> 'eltech4383',
+     'sort' => 'id_asc',
+     'offset' => 0,
+     'count' => 10,
+     'fields'=>  implode(',', $fields),
+     'access_token'=>'62730d1ed3005f721d56bb05bb234b6fc9e45dcd66acb7c45f47a6f0a3396a7e8322ca0b0ffe9f2a9aaf4'
+    ];
+ $url = "https://api.vk.com/method/groups.getMembers?" .  http_build_query($request_par);
+ $result = json_decode(file_get_contents($url),true);
+ $first_names = array();
+ //echo($result);
+ //$test_id = $result->response[0]->items[0]->city;
+ foreach ($result['response'] as $test_id) {
+    // foreach ($result['users'] as $test_user) {
+     $kek = 0;
+     while($kek < 10) {
+        $first_names[$kek] = $test_id[$kek]['first_name'];
+       
+        print_r($test_id[$kek]['first_name'].'<br>');
+        $kek++;
+     }
+     //}
+ }
+ print_r($first_names);
 ?>
+
+
+
+
+<?php
+require('vendor/autoload.php');
+
+use Everyman\Neo4j\Client,
+    Everyman\Neo4j\Transport,
+    Everyman\Neo4j\Node,
+    Everyman\Neo4j\Relationship;
+
+// Connecting to a different port or host
+
+$client = new Client();
+
+// Connecting using HTTPS and Basic Auth
+
+//$client->getTransport()
+  //->useHttps()
+  //->setAuth('Kek', 'Kek');
+//print_r($client->getServerInfo());
+//$client->getTransport()->useHttps(FALSE);
+ $person = array();
+while ($kek >= 0) {
+   $person[$kek] = new Node($client);
+   $person[$kek]->setProperty('first_name', $first_names[$kek] )->save(); 
+   $kek--;  
+}
+$kek = 0;
+while ($kek <= 10) {
+   $person[$kek]->relateTo($person[$kek+1], 'KNOWS')->save(); 
+   $kek++;
+}   
+    
+//$keanu = new Node($client);
+//$keanu->setProperty('name', 'Keanu Reeves')->save();
+//$laurence = new Node($client);
+//$laurence->setProperty('name', 'Laurence Fishburne')->save();
+//$jennifer = new Node($client);
+//$jennifer->setProperty('name', 'Jennifer Connelly')->save();
+//$kevin = new Node($client);
+//$kevin->setProperty('name', 'Kevin Bacon')->save();
+
+//$matrix = new Node($client);
+//$matrix->setProperty('title', 'The Matrix')->save();
+//$higherLearning = new Node($client);
+//$higherLearning->setProperty('title', 'Higher Learning')->save();
+//$mysticRiver = new Node($client);
+//$mysticRiver->setProperty('title', 'Mystic River')->save();
+
+?>
+
+</body>
+</html>
